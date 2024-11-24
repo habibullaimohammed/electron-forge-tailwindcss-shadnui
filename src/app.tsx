8ee6d1aea8450570a9f138a48a29b9c7784849fc
Mohammed Habibullahi Mohammed <habibullaimohammed@gmail.com>
2024-11-24 11:49:21 +0100

    step ui 1

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+// # src/app.tsx
+
+import { Menu } from "lucide-react";
+import "./index.css"; // import css
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <h1 className="font-bold text-2xl underline text-red-700">
+      Hello react <Menu />
+    </h1>
+  </React.StrictMode>
+);
