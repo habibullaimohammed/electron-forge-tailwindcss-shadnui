6bc11402e3c6854ca70289cd2effe8e5671b4857
Mohammed Habibullahi Mohammed <habibullaimohammed@gmail.com>
2024-11-24 12:10:41 +0100

    stepup UI library complete

--- src/app.tsx
+++ src/app.tsx
@@ -5,12 +5,14 @@ import "./index.css"; // import css
 
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { Button } from "./components/ui/button";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <h1 className="font-bold text-2xl underline text-red-700">
       Hello react <Menu />
+      <Button>Hi</Button>
     </h1>
   </React.StrictMode>
 );
